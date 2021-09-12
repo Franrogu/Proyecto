13523f51dc9ec5425ada62fdaa9ef2d5f83d7ff8
Franrogu <franciscoantoniorg24@gmail.com>
2021-09-11 20:45:54 -0500

    loggin-modificaciones-html,css

---
+++ src/js/Receta.js
@@ -0,0 +1,27 @@
+class Receta{
+    constructor(){
+        this.nombre ='';
+        this.tiempo = 0;
+        this.dificultad = '';
+        this.favoritos = 1;
+        this.porciones = 0;
+        this.precio = '';
+        this.estrellas = '';
+    }
+
+    ingredientes(){
+
+    }
+
+    informacionNutrimental(){
+
+    }
+
+    preparacion(){
+
+    }
+
+    miInformacionNutrimental(){
+        
+    }
+}
---
+++ src/js/usuario.js
@@ -0,0 +1,11 @@
+class Usuario{
+    constructor(nombre,apellidos, edad, sexo, peso, estatura, objetivo){
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.estatura = estatura;
+        this.objetivo = objetivo;
+    }
+}
